6ff360356a738fe9bdd678ddef54846fc13a3cea
vinothrajalingam <52484665+vinothrajalingam@users.noreply.github.com>
2025-03-10 15:45:59 +0530

    Update PowerBIApp.js

--- src/Components/PowerBIApp.js
+++ src/Components/PowerBIApp.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 const reports = [
-  { id: 1, name: "Finance Report", url: "https://app.powerbi.com/view?r=eyJrIjoiNDQ5YTA5MTctM2VlNS00NDFjLThhOWQtZmQ0YzFhYjVkZTliIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2LTg3MTIxNzdkNjY5ZSJ9" },
-  { id: 2, name: "Employee Hiring", url: "https://app.powerbi.com/view?r=eyJrIjoiY2U4ZDU3OGQtZDViMy00YzQ4LWI4ZmMtZmJiODg0ZTE5ZTFmIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2LTg3MTIxNzdkNjY5ZSJ9" },
-  { id: 3, name: "Spend Report", url: "https://app.powerbi.com/view?r=eyJrIjoiMTQ1ZmQ0MDEtMDQ1Yi00ZDc1LTk3NGEtMTA4Nzc0NzE4ZWVkIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2LTg3MTIxNzdkNjY5ZSJ9" },
+  { id: 1, name: "Finance Report", url: "https://app.powerbi.com/view?r=eyJrIjoiNDQ5YTA5MTctM2VlNS00NDFjLThhOWQtZmQ0YzFhYjVkZTliIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2L********************" },
+  { id: 2, name: "Employee Hiring", url: "https://app.powerbi.com/view?r=eyJrIjoiY2U4ZDU3OGQtZDViMy00YzQ4LWI4ZmMtZmJiODg0ZTE5ZTFmIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2L********************" },
+  { id: 3, name: "Spend Report", url: "https://app.powerbi.com/view?r=eyJrIjoiMTQ1ZmQ0MDEtMDQ1Yi00ZDc1LTk3NGEtMTA4Nzc0NzE4ZWVkIiwidCI6ImMxZjcwMGVmLThmNzktNGRlYi04NTQ2L********************" },
 ];
 
 const PowerBIApp = () => {
